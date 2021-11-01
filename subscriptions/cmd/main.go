ed2599b5efa72a25a7b6deafa69748fa168e34f5
snooyen <sean@bright.ai>
2021-10-31 17:42:38 -0700

    feat(subscriptions): sends facts

--- subscriptions/cmd/main.go
+++ subscriptions/cmd/main.go
@@ -10,14 +10,18 @@ import (
 	"github.com/snooyen/animal-facts/subscriptions/pkg/subscriptions"
 	"github.com/snooyen/animal-facts/subscriptions/pkg/version"
 	flag "github.com/spf13/pflag"
+	"github.com/twilio/twilio-go"
+	"google.golang.org/grpc"
 )
 
 var (
-	versionInfo   = flag.Bool("version", false, "prints the version information")
-	redisHost     = flag.String("redisHost", "localhost", "Hostname/address of redis")
-	redisPort     = flag.String("redisPort", "6379", "Port with which to connect to redis")
-	redisPassword = flag.String("redisPassword", "password123!", "Password to authenticate to redis")
-	redisDB       = flag.Int("redisDB", 0, "Redis DB id")
+	versionInfo     = flag.Bool("version", false, "prints the version information")
+	redisHost       = flag.String("redisHost", "localhost", "Hostname/address of redis")
+	redisPort       = flag.String("redisPort", "6379", "Port with which to connect to redis")
+	redisPassword   = flag.String("redisPassword", "password123!", "Password to authenticate to redis")
+	redisDB         = flag.Int("redisDB", 0, "Redis DB id")
+	twilioNumber    = flag.String("twilioNumber", "(555) 555-5555", "Twilio number to send messages from")
+	usersClientAddr = flag.String("usersClientAddr", "users-api:3081", "Address of users grpc service")
 )
 
 func main() {
@@ -48,10 +52,19 @@ func main() {
 		DB:       *redisDB,
 	})
 
-	sub := subscriptions.New("elephant-seal", logger, rdb)
+	twilioClient := twilio.NewRestClient()
 
 	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	conn, err := grpc.Dial(*usersClientAddr, grpc.WithInsecure())
+	if err != nil {
+		fmt.Errorf("failed to connect to users grpc service, err: %+v", err)
+		os.Exit(1)
+	}
+	defer func() {
+		cancel()
+		conn.Close()
+	}()
 
+	sub := subscriptions.New("elephant-seal", logger, rdb, conn, twilioClient, *twilioNumber)
 	sub.Start(ctx)
 }
--- subscriptions/pkg/subscriptions/service.go
+++ subscriptions/pkg/subscriptions/service.go
@@ -2,10 +2,17 @@ package subscriptions
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/go-kit/log"
 	"github.com/go-redis/redis/v8"
+	"github.com/snooyen/animal-facts/users/pb"
+	"github.com/twilio/twilio-go"
+	openapi "github.com/twilio/twilio-go/rest/api/v2010"
+	"google.golang.org/grpc"
 )
 
 const (
@@ -13,22 +20,28 @@ const (
 )
 
 type subscription struct {
-	Animal string
-	rdb    *redis.Client
-	logger log.Logger
+	Animal       string
+	rdb          *redis.Client
+	logger       log.Logger
+	twilio       *twilio.RestClient
+	twilioNumber string
+	users        pb.UsersClient
 }
 
-func New(animal string, logger log.Logger, rdb *redis.Client) *subscription {
+func New(animal string, logger log.Logger, rdb *redis.Client, usersConn *grpc.ClientConn, twilioClient *twilio.RestClient, twilioNumber string) *subscription {
 	return &subscription{
-		Animal: animal,
-		logger: logger,
-		rdb:    rdb,
+		Animal:       animal,
+		logger:       logger,
+		rdb:          rdb,
+		users:        pb.NewUsersClient(usersConn),
+		twilio:       twilioClient,
+		twilioNumber: twilioNumber,
 	}
 }
 
 func (s *subscription) Start(ctx context.Context) error {
 	chName := subscriptionKey + ":" + s.Animal
-	s.logger.Log("subscribing to %s", chName)
+	s.logger.Log("method", "Start", "message", "start subscription", "channel", chName)
 	ch := s.rdb.Subscribe(ctx, chName).Channel()
 
 	rateLimit := time.NewTicker(1 * time.Second)
@@ -40,8 +53,46 @@ func (s *subscription) Start(ctx context.Context) error {
 		case <-ctx.Done():
 			return nil
 		default:
-			s.logger.Log("received message: %s", msg.Payload)
+			s.logger.Log("method", "Start", "channel", chName, "message", msg.Payload)
+			subscribers, err := s.rdb.SMembers(ctx, "subscribers:"+s.Animal).Result()
+			if err != nil {
+				s.logger.Log("method", "Start", "channel", chName, "error", err)
+			}
+			for _, subscriber := range subscribers {
+				go s.sendFact(ctx, subscriber, msg.Payload)
+			}
 		}
 	}
 	return nil
+}
+
+func (s *subscription) sendFact(ctx context.Context, subscriber string, fact string) {
+	// Parse subscriber string to uuid and get user
+	uuid, err := strconv.Atoi(subscriber)
+	if err != nil {
+		s.logger.Log("method", "sendFact", "error", err)
+		return
+	}
+	r, err := s.users.GetUser(ctx, &pb.GetUserRequest{ID: int64(uuid)})
+	if err != nil {
+		s.logger.Log("method", "sendFact", "error", err)
+	}
+
+	// Send fact to user
+	s.logger.Log("method", "sendFact", "user", r.GetPhone(), "fact", fact)
+	date := time.Now().Format("2006-01-02")
+	msg := fmt.Sprintf("%s's %s fact for %s\n\n%s", r.GetName(), s.Animal, date, fact)
+	params := &openapi.CreateMessageParams{}
+	params.SetFrom(s.twilioNumber)
+	params.SetTo(r.GetPhone())
+
+	//TODO: Check if we need to send a welcome message
+
+	params.SetBody(msg)
+	rsp, err := s.twilio.ApiV2010.CreateMessage(params)
+	if err != nil {
+		s.logger.Log("method", "sendFact", "error", err, "params", params)
+	}
+	response, _ := json.Marshal(*rsp)
+	s.logger.Log("method", "sendFact", "user", r.GetPhone(), "response", string(response))
 }
